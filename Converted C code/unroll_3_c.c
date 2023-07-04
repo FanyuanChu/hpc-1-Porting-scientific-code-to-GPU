07441ce186ead7bfe213e2d56d07bac79c912245
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-04 10:49:58 +0100

    Update unroll_3_c.c

--- Converted C code/unroll_3_c.c
+++ Converted C code/unroll_3_c.c
@@ -25,7 +25,6 @@
 #define CN4S___SOR_MAP_70_SCAL 0.5
 #define OMEGA___SOR_MAP_70_SCAL 1
 
-
 void stage_kernel_1(int global_id_0, float* p0_0, float* rhs_0, float* p3_1) {
     int i___sor_map_70_scal;
     int j___sor_map_70_scal;
@@ -37,9 +36,30 @@ void stage_kernel_1(int global_id_0, float* p0_0, float* rhs_0, float* p3_1) {
     int i_rel___sor_map_70_scal;
     int j_rel___sor_map_70_scal;
     int k_rel___sor_map_70_scal;
+    //
 
+    int s1[10] = {0, 103, 10405, 10407, 10506, 10507, 10508, 10508, 10606, 10606};
 
-    float p0_0[853129];
-    float rhs_0[853129];
-    float p3_1[853129];
-  
+    // if (global_id_0 < 853129) would be used in OpenCL. Here we loop instead
+    for (global_id_0 = 0; global_id_0 < 853129; global_id_0++) {
+        if (global_id_0 >= s1[1] && global_id_0 < s1[2]) {
+            // Calculate i___sor_map_22_scal, j___sor_map_22_scal, k___sor_map_22_scal
+            // based on global_id_0
+            // Then perform calculation for this case
+            // 
+        }
+        else if (global_id_0 >= s1[2] && global_id_0 < s1[3]) {
+            // Calculate i___sor_map_46_scal, j___sor_map_46_scal, k___sor_map_46_scal
+            // based on global_id_0
+            // Then perform calculation for this case
+            // 
+        }
+        else if (global_id_0 >= s1[3] && global_id_0 < s1[4]) {
+            // Calculate i___sor_map_70_scal, j___sor_map_70_scal, k___sor_map_70_scal
+            // based on global_id_0
+            // Then perform calculation for this case
+            // 
+        }
+        // 
+    }
+}
