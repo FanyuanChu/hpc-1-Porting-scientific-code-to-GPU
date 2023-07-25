f470f7aa17265a2353357ad8e9385ce760b74f3e
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 07:41:59 +0100

    Update sor_routines.f95

--- SOR_unroll_GPU/src/sor_routines.f95
+++ SOR_unroll_GPU/src/sor_routines.f95
@@ -2,7 +2,7 @@ module sor_routines
     use sor_params
     contains
 
-    attributes(global) subroutine sor(p0_d,p1_d,rhs_d)
+    attributes(global) subroutine sor(p0_d, p1_d, rhs_d)
         use sor_params
         real, dimension(:,:,:), device :: p0_d, p1_d, rhs_d
         integer :: i,j,k
@@ -25,21 +25,9 @@ module sor_routines
         j = (index - k * (im+1) * (jm+1)) / (im+1)
         i = index - k * (im+1) * (jm+1) - j * (im+1)
 
-        if (i==im+1) then
-            p1_d(i,j,k) = p0_d(i-im,j,k)
-        else if (i==0) then
-            p1_d(i,j,k) = p0_d(i+im,j,k)
-        else if (j==jm+1) then
-            p1_d(i,j,k)=p0_d(i-1,j,k)
-        else if (j==0) then
-            p1_d(i,j,k)=p0_d(i,j,k)
-        else if (k==0) then
-            p1_d(i,j,k)=p0_d(i,j,k)
-        else if (k==km+1) then
-            p1_d(i,j,k)=p0_d(i,j,k)
-        else
+        if (i > 0 .and. i < im+1 .and. j > 0 .and. j < jm+1 .and. k > 0 .and. k < km+1) then
             reltmp = omega*(cn1 *(cn2l*p0_d(i+1,j,k) + cn2s*p0_d(i-1,j,k) + cn3l*p0_d(i,j+1,k) + cn3s*p0_d(i,j-1,k) + cn4l*p0_d(i,j,k+1) + cn4s*p0_d(i,j,k-1) -rhs_d(i,j,k))-p0_d(i,j,k))
-            p1_d(i,j,k) = p0_d(i,j,k) +reltmp    
+            p1_d(i,j,k) = p0_d(i,j,k) + reltmp
         end if
     end subroutine sor
 
@@ -47,13 +35,9 @@ module sor_routines
         real, dimension(:,:,:), device :: A, B
         real, dimension(:,:,:), device, allocatable :: temp
 
-        allocate(temp(0:im+1,0:jm+1,0:km+1))
-
         temp = A
         A = B
         B = temp
-
-        deallocate(temp)
     end subroutine swap
 
 end module sor_routines
