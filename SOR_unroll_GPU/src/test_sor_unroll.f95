5a26d0efb003a1684482d327694ae495268674cb
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 07:21:58 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,84 +1,73 @@
 program test_sor_unroll
-#ifdef WITH_OPENMP
-use omp_lib
-#endif
+    use omp_lib
+    use sor_params
+    use sor_routines
 
-use sor_params
-use sor_routines
-
-! CHANGED Declare device arrays
-real, dimension(:,:,:), device, allocatable :: p0_d, p1_d, rhs_d
-
-! CHANGED Declare host arrays
-real, dimension(:,:,:), allocatable :: p0_h, p1_h, rhs_h
-
-integer :: iter, niters
-integer :: i,j,k
-#ifdef TIMING
+    real, dimension(:,:,:), allocatable :: p0_h, p1_h, rhs_h
+    real, dimension(:,:,:), device, allocatable :: p0_d, p1_d, rhs_d
+    integer :: iter, niters
+    integer :: i, j, k, blocks
     integer :: clock_rate
     integer, dimension(0:1) :: timestamp
-#endif
 
-! CHANGED Allocate device arrays
-allocate(p0_d(0:im+1,0:jm+1,0:km+1))
-allocate(p1_d(0:im+1,0:jm+1,0:km+1))
-allocate(rhs_d(0:im+1,0:jm+1,0:km+1))
+    ! Allocate host arrays
+    allocate(p0_h(0:im+1,0:jm+1,0:km+1))
+    allocate(p1_h(0:im+1,0:jm+1,0:km+1))
+    allocate(rhs_h(0:im+1,0:jm+1,0:km+1))
 
-! CHANGED Allocate host arrays
-allocate(p0_h(0:im+1,0:jm+1,0:km+1))
-allocate(p1_h(0:im+1,0:jm+1,0:km+1))
-allocate(rhs_h(0:im+1,0:jm+1,0:km+1))
+    ! Initialize host arrays
+    do i = 0,im+1
+        do j = 0,jm+1
+            do k = 0,km+1
+                rhs_h(i,j,k) = 1.0
+                p0_h(i,j,k) = 1.0
+            end do
+        end do
+    end do
 
-do i = 0,im+1
-do j = 0,jm+1
-do k = 0,km+1
-    rhs_h(i,j,k) = 1.0
-    p0_h(i,j,k) = 1.0
-end do
-end do
-end do
+    ! Calculate the number of blocks
+    integer, parameter :: threadsPerBlock = 256
+    blocks = ((im+1) * (jm+1) * (km+1) + threadsPerBlock - 1) / threadsPerBlock
 
-! CHANGED Copy data from host to device
-rhs_d = rhs_h
-p0_d = p0_h
+    ! Allocate device memory
+    allocate(p0_d(0:im+1,0:jm+1,0:km+1))
+    allocate(p1_d(0:im+1,0:jm+1,0:km+1))
+    allocate(rhs_d(0:im+1,0:jm+1,0:km+1))
 
-niters = 12/UNROLL
-#ifdef TIMING
+    ! Copy data from host to device
+    p0_d = p0_h
+    rhs_d = rhs_h
+
+    niters = 12/UNROLL
     call system_clock(timestamp(0), clock_rate)
-#endif
 
-do iter = 1,niters
-    print *,iter
+    do iter = 1,niters
+        print *,iter
 
-! CHANGED Call CUDA kernel
-    call sor(p0_d,p1_d,rhs_d)
+        call sor<<<blocks, threadsPerBlock>>>(p0_d, p1_d, rhs_d)
+        call cudaDeviceSynchronize()
 
-! CHANGED Swap pointers for next iteration
-    if (mod(iter, 2) == 0) then
-        call swap(p0_d, p1_d)
-    end if
-end do
+        if (mod(iter, 2) == 0) then
+            call swap(p0_d, p1_d)
+        end if
+    end do
 
-! CHANGED Copy results from device to host
-p1_h = p1_d
+    ! Copy results from device to host
+    p1_h = p1_d
 
-#ifdef TIMING
     call system_clock(timestamp(1), clock_rate)
     print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)
-#endif
 
-print *, p1_h(im/2,jm/2,km/2)
+    print *, p1_h(im/2,jm/2,km/2)
 
-! CHANGED Deallocate device arrays
-deallocate(p0_d)
-deallocate(p1_d)
-deallocate(rhs_d)
+    ! Free device memory
+    deallocate(p0_d)
+    deallocate(p1_d)
+    deallocate(rhs_d)
 
-! CHANGED Deallocate host arrays
-deallocate(p0_h)
-deallocate(p1_h)
-deallocate(rhs_h)
+    ! Deallocate host arrays
+    deallocate(p0_h)
+    deallocate(p1_h)
+    deallocate(rhs_h)
 
 end program test_sor_unroll
-
-
