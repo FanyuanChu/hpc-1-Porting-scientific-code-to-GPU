a93b6ee7c75e243561b0ccf913f65c685005296f
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 03:15:20 +0100

    Update sor_routines.f95

--- SOR_unroll_GPU/src/sor_routines.f95
+++ SOR_unroll_GPU/src/sor_routines.f95
@@ -1,4 +1,3 @@
-
 module sor_routines
 use sor_params
 contains
@@ -9,27 +8,27 @@ real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: p0
 real, dimension(0:im+1,0:jm+1,0:km+1), intent(Out) :: p1
 real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: rhs 
 integer :: i,j,k
-#ifdef WITH_OPENMP
-!$OMP PARALLEL DO
-#endif
-do i = 0,im+1
-do j = 0,jm+1
-do k = 0,km+1
-call sor_kernel(p0,p1,rhs,i,j,k)
-end do
-end do
-end do
-#ifdef WITH_OPENMP
-!$OMP END PARALLEL DO
-#endif
+integer, parameter :: threadsPerBlock = 256  // Modified part
+integer :: blocks  // Modified part
+
+// Calculate the number of blocks
+blocks = ((im+1) * (jm+1) * (km+1) + threadsPerBlock - 1) / threadsPerBlock  // Modified part
+
+// Launch the kernel
+call sor_kernel<<<blocks, threadsPerBlock>>>(p0, p1, rhs)  // Modified part
+
+// Make sure all threads have finished before continuing
+call cudaDeviceSynchronize()  // Modified part
+
 end subroutine sor
 
-subroutine sor_kernel(p0,p1,rhs,i,j,k) 
+attributes(global) subroutine sor_kernel(p0,p1,rhs)  // Modified part
     use sor_params
 real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: p0
 real, dimension(0:im+1,0:jm+1,0:km+1), intent(Out) :: p1
 real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: rhs 
-integer, intent(In) :: i,j,k
+integer :: i,j,k
+integer :: index  // Modified part
 real(kind=4), parameter :: cn1 = 1.0/3.0
 real(kind=4), parameter :: cn2l = 0.5
 real(kind=4), parameter :: cn2s = 0.5
@@ -40,7 +39,15 @@ real(kind=4), parameter :: cn4s = 0.5
 real, parameter  :: omega = 1.0
 real :: reltmp
 
-! assume i=x =  west to east , y=j=south to north, k=z = vertical
+index = blockDim%x * blockIdx%x + threadIdx%x  // Modified part
+
+if (index > (im+1) * (jm+1) * (km+1)) return  // Modified part
+
+k = index / ((im+1) * (jm+1))  // Modified part
+j = (index - k * (im+1) * (jm+1)) / (im+1)  // Modified part
+i = index - k * (im+1) * (jm+1) - j * (im+1)  // Modified part
+
+// Rest of the code
 if (i==im+1) then
 ! circular
 ! i=im+1
@@ -57,19 +64,19 @@ else if (j==0) then
 ! fixed
 ! j = 0
 ! We keep the original values
-    p1(i,j,k)=p0(i,j,k)
+p1(i,j,k)=p0(i,j,k)
 else if (k==0) then
-    p1(i,j,k)=p0(i,j,k)
+p1(i,j,k)=p0(i,j,k)
 else if (k==km+1) then
-    p1(i,j,k)=p0(i,j,k)
+p1(i,j,k)=p0(i,j,k)
 else
 ! the core
 ! The actual SOR expression
-    reltmp = omega*(cn1 *(cn2l*p0(i+1,j,k) + &
-        cn2s*p0(i-1,j,k) +cn3l*p0(i,j+1,k) + &
-        cn3s*p0(i,j-1,k) +cn4l*p0(i,j,k+1) + &
-        cn4s*p0(i,j,k-1) -rhs(i,j,k))-p0(i,j,k))
-    p1(i,j,k) = p0(i,j,k) +reltmp    
+reltmp = omega*(cn1 *(cn2l*p0(i+1,j,k) + &
+cn2s*p0(i-1,j,k) +cn3l*p0(i,j+1,k) + &
+cn3s*p0(i,j-1,k) +cn4l*p0(i,j,k+1) + &
+cn4s*p0(i,j,k-1) -rhs(i,j,k))-p0(i,j,k))
+p1(i,j,k) = p0(i,j,k) +reltmp    
 end if
 
 end subroutine sor_kernel
