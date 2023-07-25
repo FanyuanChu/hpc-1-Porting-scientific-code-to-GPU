d573b8065762de35aaf40f2ec34041cb872a69b5
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 03:16:00 +0100

    Update sor_params.f95

--- SOR_unroll_GPU/src/sor_params.f95
+++ SOR_unroll_GPU/src/sor_params.f95
@@ -2,4 +2,9 @@ module sor_params
 integer, parameter :: im=1000
 integer, parameter :: jm=1000
 integer, parameter :: km=320
+! New parameters to define the block size
+integer, parameter :: tx = 16
+integer, parameter :: ty = 16
+integer, parameter :: tz = 16
 end module sor_params
+
