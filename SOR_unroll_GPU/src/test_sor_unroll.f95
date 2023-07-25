180ed133b5e2ac1623d839511a432944929695b6
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 03:13:54 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -35,113 +35,80 @@ use omp_lib
 
 use sor_params
 use sor_routines
-#ifndef DYN_ALLOC
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p0
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p1
-real, dimension(0:im+1,0:jm+1,0:km+1) :: rhs
-#if UNROLL>1
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p2
-#if UNROLL>2
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p3
-#if UNROLL>3
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p4
-#endif
-#endif
-#endif
-#else
-    real, allocatable  :: p0(:,:,:)
-    real, allocatable  :: p1(:,:,:)
-    real, allocatable  :: rhs(:,:,:)
-#if UNROLL>1
-    real, allocatable  :: p2(:,:,:)
-#if UNROLL>2
-    real, allocatable  :: p3(:,:,:)
-#if UNROLL>3
-    real, allocatable  :: p4(:,:,:)
-#endif
-#endif
-#endif
-#endif
-integer :: iter, niters
 
+!<<CHANGED>> Declare device arrays
+real, dimension(:,:,:), device, allocatable :: p0_d, p1_d, rhs_d
+
+!<<CHANGED>> Declare host arrays
+real, dimension(:,:,:), allocatable :: p0_h, p1_h, rhs_h
+
+integer :: iter, niters
 integer :: i,j,k
 #ifdef TIMING
     integer :: clock_rate
     integer, dimension(0:1) :: timestamp
 #endif
 
-#ifdef DYN_ALLOC
-    allocate(p0(0:im+1,0:jm+1,0:km+1))
-    allocate(p1(0:im+1,0:jm+1,0:km+1))
-    allocate(rhs(0:im+1,0:jm+1,0:km+1))
-#if UNROLL>1
-    allocate(p2(0:im+1,0:jm+1,0:km+1))
-#if UNROLL>2
-    allocate(p3(0:im+1,0:jm+1,0:km+1))
-#if UNROLL>3
-    allocate(p4(0:im+1,0:jm+1,0:km+1))
-#endif
-#endif
-#endif
-#endif
+!<<CHANGED>> Allocate device arrays
+allocate(p0_d(0:im+1,0:jm+1,0:km+1))
+allocate(p1_d(0:im+1,0:jm+1,0:km+1))
+allocate(rhs_d(0:im+1,0:jm+1,0:km+1))
+
+!<<CHANGED>> Allocate host arrays
+allocate(p0_h(0:im+1,0:jm+1,0:km+1))
+allocate(p1_h(0:im+1,0:jm+1,0:km+1))
+allocate(rhs_h(0:im+1,0:jm+1,0:km+1))
 
 do i = 0,im+1
 do j = 0,jm+1
 do k = 0,km+1
-    rhs(i,j,k) = 1.0
-    p0(i,j,k) = 1.0
+    rhs_h(i,j,k) = 1.0
+    p0_h(i,j,k) = 1.0
 end do
 end do
 end do
+
+!<<CHANGED>> Copy data from host to device
+rhs_d = rhs_h
+p0_d = p0_h
 
 niters = 12/UNROLL
 #ifdef TIMING
     call system_clock(timestamp(0), clock_rate)
 #endif
+
 do iter = 1,niters
     print *,iter
-!$RF4A Subroutine sor_superkernel
-!$RF4A Begin Inline
-call sor (p0,p1,rhs)
-#if UNROLL > 1
-call sor (p1,p2,rhs)
-#if UNROLL > 2
-call sor (p2,p3,rhs)
-#if UNROLL > 3
-call sor (p3,p4,rhs)
-#endif
-#endif
-#endif
-!$RF4A End Inline
-!$RF4A End Subroutine sor_superkernel
-#if UNROLL==1
-p0=p1
-#elif UNROLL==2
-p0=p2
-#elif UNROLL==3
-p0=p3
-#elif UNROLL==4
-p0=p4
-#endif
+
+!<<CHANGED>> Call CUDA kernel
+    call sor(p0_d,p1_d,rhs_d)
+
+!<<CHANGED>> Swap pointers for next iteration
+    if (mod(iter, 2) == 0) then
+        call swap(p0_d, p1_d)
+    end if
 end do
+
+!<<CHANGED>> Copy results from device to host
+p1_h = p1_d
+
 #ifdef TIMING
     call system_clock(timestamp(1), clock_rate)
     print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)
 #endif
-print *, p0(im/2,jm/2,km/2)
-#ifdef DYN_ALLOC
-    deallocate(p0)
-    deallocate(p1)
-    deallocate(rhs)
-#if UNROLL>1
-    deallocate(p2)
-#if UNROLL>2
-    deallocate(p3)
-#if UNROLL>3
-    deallocate(p4)
-#endif
-#endif
-#endif
-#endif
-end program
+
+print *, p1_h(im/2,jm/2,km/2)
+
+!<<CHANGED>> Deallocate device arrays
+deallocate(p0_d)
+deallocate(p1_d)
+deallocate(rhs_d)
+
+!<<CHANGED>> Deallocate host arrays
+deallocate(p0_h)
+deallocate(p1_h)
+deallocate(rhs_h)
+
+end program test_sor_unroll
+
 
