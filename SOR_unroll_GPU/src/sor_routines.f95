1f9614d8f8056586c9790c5e132412fc9a414b33
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 08:17:40 +0100

    Update sor_routines.f95

--- SOR_unroll_GPU/src/sor_routines.f95
+++ SOR_unroll_GPU/src/sor_routines.f95
@@ -1,13 +1,4 @@
-! sor_params.f95
-module sor_params
-integer, parameter :: im=1000
-integer, parameter :: jm=1000
-integer, parameter :: km=320
-! New parameters to define the block size
-integer, parameter :: tx = 16
-integer, parameter :: ty = 16
-integer, parameter :: tz = 16
-end module sor_params                                                                                                                                                                  ! sor_routines.f95
+! sor_routines.f95
 module sor_routines
     use sor_params
     contains
