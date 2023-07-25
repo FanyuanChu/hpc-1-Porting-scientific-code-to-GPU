bde1752af5388156b980f018425fa9a061f9116a
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 03:07:54 +0100

    Delete Converted C code directory

--- Converted C code/src_C/sor_routines.h
+++
@@ -1,9 +0,0 @@
-// sor_routines.h
-#include "sor_params.h"
-#include <omp.h>
-
-void sor(float *p0, float *p1, float *rhs);
-void sor_kernel(float *p0, float *p1, float *rhs, int i, int j, int k);
-
-
-
