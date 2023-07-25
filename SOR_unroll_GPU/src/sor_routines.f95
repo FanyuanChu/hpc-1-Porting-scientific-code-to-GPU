8d136b66dc2e41778fd5f5813011d017c5425237
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-25 07:17:51 +0100

    Update sor_routines.f95

--- SOR_unroll_GPU/src/sor_routines.f95
+++ SOR_unroll_GPU/src/sor_routines.f95
@@ -2,48 +2,7 @@ module sor_routines
 use sor_params
 contains
 
-subroutine sor (p0,p1,rhs)
-    use sor_params
-    real, dimension(:,:,:), intent(In) :: p0
-    real, dimension(:,:,:), intent(Out) :: p1
-    real, dimension(:,:,:), intent(In) :: rhs 
-    integer :: i,j,k
-
-    ! Modified part
-    integer, parameter :: threadsPerBlock = 256
-    integer :: blocks
-
-    ! Declare device copies of the arrays
-    real, dimension(:,:,:), device, allocatable :: p0_d, p1_d, rhs_d
-
-    ! Calculate the number of blocks
-    blocks = ((im+1) * (jm+1) * (km+1) + threadsPerBlock - 1) / threadsPerBlock
-
-    ! Allocate memory on the device
-    allocate(p0_d(0:im+1,0:jm+1,0:km+1))
-    allocate(p1_d(0:im+1,0:jm+1,0:km+1))
-    allocate(rhs_d(0:im+1,0:jm+1,0:km+1))
-
-    ! Copy data from host to device
-    p0_d = p0
-    p1_d = p1
-    rhs_d = rhs
-
-    ! Launch the kernel
-    call sor_kernel<<<blocks, threadsPerBlock>>>(p0_d, p1_d, rhs_d)
-
-    ! Make sure all threads have finished before continuing
-    call cudaDeviceSynchronize()
-
-    ! Copy results from device to host
-    p1 = p1_d
-
-    ! Free device memory
-    deallocate(p0_d, p1_d, rhs_d)
-
-end subroutine sor
-
-attributes(global) subroutine sor_kernel(p0_d,p1_d,rhs_d)
+attributes(global) subroutine sor(p0_d,p1_d,rhs_d)
     use sor_params
     real, dimension(:,:,:), device :: p0_d, p1_d, rhs_d
     integer :: i,j,k
@@ -82,6 +41,6 @@ attributes(global) subroutine sor_kernel(p0_d,p1_d,rhs_d)
         reltmp = omega*(cn1 *(cn2l*p0_d(i+1,j,k) + cn2s*p0_d(i-1,j,k) + cn3l*p0_d(i,j+1,k) + cn3s*p0_d(i,j-1,k) + cn4l*p0_d(i,j,k+1) + cn4s*p0_d(i,j,k-1) -rhs_d(i,j,k))-p0_d(i,j,k))
         p1_d(i,j,k) = p0_d(i,j,k) +reltmp    
     end if
-end subroutine sor_kernel
+end subroutine sor
 
 end module sor_routines
