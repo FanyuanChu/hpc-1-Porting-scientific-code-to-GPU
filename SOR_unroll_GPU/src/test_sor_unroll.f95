4cc553c1bf254897697b7252f7c9751d8ef9f799
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 19:43:20 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,21 +1,20 @@
+! test_sor_unroll.f95 
 program test_sor_unroll
     use sor_params
-    use sor_routines
+    use sor_routines  !!! ADDED
 #ifndef DYN_ALLOC
-    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p0
-    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p1
-    real, dimension(0:im+1,0:jm+1,0:km+1), device :: rhs
+    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p0  !!! MODIFIED
+    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p1  !!! MODIFIED
+    real, dimension(0:im+1,0:jm+1,0:km+1), device :: rhs  !!! MODIFIED
 #else
-    real, device, allocatable  :: p0(:,:,:)  
-    real, device, allocatable  :: p1(:,:,:)  
-    real, device, allocatable  :: rhs(:,:,:)  
+    real, device, allocatable  :: p0(:,:,:)  !!! MODIFIED
+    real, device, allocatable  :: p1(:,:,:)  !!! MODIFIED
+    real, device, allocatable  :: rhs(:,:,:)  !!! MODIFIED
 #endif
     integer :: iter, niters
-    real, dimension(0:im+1,0:jm+1,0:km+1) :: p0_host  ! Moved up
+
     integer :: i,j,k
 
-
-    print *, "Initializing arrays..."
     do i = 0,im+1
     do j = 0,jm+1
     do k = 0,km+1
@@ -24,22 +23,16 @@ program test_sor_unroll
     end do
     end do
     end do
-    print *, "Arrays initialized."
 
     niters = 12/UNROLL
-    print *, "Starting iterations..."
+
     do iter = 1,niters
-        print *, "Iteration ", iter
+        print *,iter
         call sor (p0,p1,rhs)
     #if UNROLL==1
         p0=p1
     #endif
     end do
-    print *, "Iterations completed."
 
-    ! Copy data from device to host and print
-    print *, "Copying data from device to host..."
-    p0_host = p0
-    print *, "Data copied."
-    print *, "Value at center: ", p0_host(im/2,jm/2,km/2)
+    ! print *, p0(im/2,jm/2,km/2)  !!! MODIFIED
 end program test_sor_unroll
