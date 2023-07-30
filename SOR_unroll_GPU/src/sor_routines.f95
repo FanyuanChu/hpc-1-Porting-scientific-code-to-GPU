1ada6a3371993026e68705b8fd6a76aaf3743e8a
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 22:37:30 +0100

    Update sor_routines.f95

--- SOR_unroll_GPU/src/sor_routines.f95
+++ SOR_unroll_GPU/src/sor_routines.f95
@@ -1,31 +1,13 @@
 module sor_routines
 use sor_params
-
 contains
 
-attributes(global) subroutine sor (d_p0,d_p1,d_rhs)
-    use sor_params    
-real, dimension(0:im+1,0:jm+1,0:km+1), intent(In), device :: d_p0
-real, dimension(0:im+1,0:jm+1,0:km+1), intent(Out), device :: d_p1
-real, dimension(0:im+1,0:jm+1,0:km+1), intent(In), device :: d_rhs
-integer :: i,j,k
-
-i = (blockIdx%x-1)*blockDim%x + threadIdx%x
-j = (blockIdx%y-1)*blockDim%y + threadIdx%y
-k = (blockIdx%z-1)*blockDim%z + threadIdx%z
-
-if(i<=im+1 .and. j<=jm+1 .and. k<=km+1) then
-  call sor_kernel(d_p0,d_p1,d_rhs,i,j,k)
-endif
-
-end subroutine sor
-
-attributes(device) subroutine sor_kernel(d_p0,d_p1,d_rhs,i,j,k) 
+attributes(global) subroutine sor_kernel(p0,p1,rhs,i,j,k) 
     use sor_params
-real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: d_p0
-real, dimension(0:im+1,0:jm+1,0:km+1), intent(Out) :: d_p1
-real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: d_rhs 
-integer, intent(In) :: i,j,k
+real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(In) :: p0
+real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(Out) :: p1
+real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(In) :: rhs 
+integer, value, intent(In) :: i,j,k
 real(kind=4), parameter :: cn1 = 1.0/3.0
 real(kind=4), parameter :: cn2l = 0.5
 real(kind=4), parameter :: cn2s = 0.5
@@ -36,13 +18,13 @@ real(kind=4), parameter :: cn4s = 0.5
 real, parameter  :: omega = 1.0
 real :: reltmp
 
-if(i > 0 .and. i < im+1 .and. j > 0 .and. j < jm+1 .and. k > 0 .and. k < km+1) then
-    reltmp = d_rhs(i,j,k) - cn4l*(d_p0(i+1,j,k) + d_p0(i-1,j,k)) - cn4s*(d_p0(i+2,j,k) + d_p0(i-2,j,k)) &
-        - cn2l*(d_p0(i,j+1,k) + d_p0(i,j-1,k)) - cn2s*(d_p0(i,j+2,k) + d_p0(i,j-2,k)) &
-        - cn3l*(d_p0(i,j,k+1) + d_p0(i,j,k-1)) - cn3s*(d_p0(i,j,k+2) + d_p0(i,j,k-2)) &
-        - cn1*d_p0(i,j,k)
-    d_p1(i,j,k) = d_p0(i,j,k) + omega*reltmp
-endif
+! the core
+! The actual SOR expression
+    reltmp = omega*(cn1 *(cn2l*p0(i+1,j,k) + &
+        cn2s*p0(i-1,j,k) +cn3l*p0(i,j+1,k) + &
+        cn3s*p0(i,j-1,k) +cn4l*p0(i,j,k+1) + &
+        cn4s*p0(i,j,k-1) -rhs(i,j,k))-p0(i,j,k))
+    p1(i,j,k) = p0(i,j,k) +reltmp    
 
 end subroutine sor_kernel
 
