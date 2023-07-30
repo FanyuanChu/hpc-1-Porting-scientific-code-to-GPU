06fc862bb895007ccf842ea7aa2a01e445a7ef71
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 16:02:53 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,78 +1,62 @@
 ! test_sor_unroll.f95 
-program test_sor_unroll
-    use omp_lib
-    use sor_params
-    use sor_routines
+ program test_sor_unroll
+#ifdef WITH_OPENMP
+use omp_lib
+#endif
 
-    real, dimension(:,:,:), allocatable :: p0_h, p1_h, rhs_h
-    real, dimension(:,:,:), device, allocatable :: p0_d, p1_d, rhs_d
-    integer :: iter, niters
-    integer :: i, j, k, blocks, grid
+use sor_params
+use sor_routines
+#ifndef DYN_ALLOC
+real, dimension(0:im+1,0:jm+1,0:km+1), device :: p0  !!! MODIFIED
+real, dimension(0:im+1,0:jm+1,0:km+1), device :: p1  !!! MODIFIED
+real, dimension(0:im+1,0:jm+1,0:km+1), device :: rhs  !!! MODIFIED
+#else
+    real, device, allocatable  :: p0(:,:,:)  !!! MODIFIED
+    real, device, allocatable  :: p1(:,:,:)  !!! MODIFIED
+    real, device, allocatable  :: rhs(:,:,:)  !!! MODIFIED
+#endif
+integer :: iter, niters
+
+integer :: i,j,k
+#ifdef TIMING
     integer :: clock_rate
     integer, dimension(0:1) :: timestamp
-    integer, dimension(3) :: block_dim, grid_dim
-    real, dimension(:,:,:), device, allocatable :: temp   ! new variable
+#endif
 
-    ! Initialize block and grid dimensions
-    block_dim = [256, 1, 1]
-    grid_dim = [(im+1+block_dim(1)-1)/block_dim(1), (jm+1+block_dim(2)-1)/block_dim(2), (km+1+block_dim(3)-1)/block_dim(3)]
+#ifdef DYN_ALLOC
+    allocate(p0(0:im+1,0:jm+1,0:km+1))
+    allocate(p1(0:im+1,0:jm+1,0:km+1))
+    allocate(rhs(0:im+1,0:jm+1,0:km+1))
+#endif
 
-    ! Allocate host arrays
-    allocate(p0_h(0:im+1,0:jm+1,0:km+1))
-    allocate(p1_h(0:im+1,0:jm+1,0:km+1))
-    allocate(rhs_h(0:im+1,0:jm+1,0:km+1))
+do i = 0,im+1
+do j = 0,jm+1
+do k = 0,km+1
+    rhs(i,j,k) = 1.0
+    p0(i,j,k) = 1.0
+end do
+end do
+end do
 
-    ! Initialize host arrays
-    do i = 0,im+1
-        do j = 0,jm+1
-            do k = 0,km+1
-                rhs_h(i,j,k) = 1.0
-                p0_h(i,j,k) = 1.0
-            end do
-        end do
-    end do
-
-    ! Allocate device memory
-    allocate(p0_d(0:im+1,0:jm+1,0:km+1))
-    allocate(p1_d(0:im+1,0:jm+1,0:km+1))
-    allocate(rhs_d(0:im+1,0:jm+1,0:km+1))
-
-    ! Copy data from host to device
-    p0_d = p0_h
-    rhs_d = rhs_h
-
-    niters = 12/UNROLL
+niters = 12/UNROLL
+#ifdef TIMING
     call system_clock(timestamp(0), clock_rate)
-
-    do iter = 1,niters
-        print *,iter
-
-        call sor<<<grid_dim, block_dim>>>(p0_d, p1_d, rhs_d)
-        call cudaDeviceSynchronize()
-
-        if (mod(iter, 2) == 0) then
-            temp = p0_d
-            p0_d = p1_d
-            p1_d = temp
-        end if
-    end do
-
-    ! Copy results from device to host
-    p1_h = p1_d
-
+#endif
+do iter = 1,niters
+    print *,iter
+call sor (p0,p1,rhs)
+#if UNROLL==1
+p0=p1
+#endif
+end do
+#ifdef TIMING
     call system_clock(timestamp(1), clock_rate)
     print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)
-
-    print *, p1_h(im/2,jm/2,km/2)
-
-    ! Free device memory
-    deallocate(p0_d)
-    deallocate(p1_d)
-    deallocate(rhs_d)
-
-    ! Deallocate host arrays
-    deallocate(p0_h)
-    deallocate(p1_h)
-    deallocate(rhs_h)
-
-end program test_sor_unroll
+#endif
+print *, p0(im/2,jm/2,km/2)
+#ifdef DYN_ALLOC
+    deallocate(p0)
+    deallocate(p1)
+    deallocate(rhs)
+#endif
+end program
