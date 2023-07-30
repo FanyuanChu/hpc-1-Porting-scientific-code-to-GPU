6f05f210807434d1b08496c7f63d0f1758dd3672
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 18:45:10 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -2,6 +2,7 @@
 program test_sor_unroll
     use sor_params
     use sor_routines
+
 #ifndef DYN_ALLOC
     real, dimension(0:im+1,0:jm+1,0:km+1), device :: p0
     real, dimension(0:im+1,0:jm+1,0:km+1), device :: p1
@@ -12,6 +13,7 @@ program test_sor_unroll
     real, device, allocatable  :: rhs(:,:,:)  
 #endif
     integer :: iter, niters
+
     integer :: i,j,k
     integer :: clock_rate  !!! ADDED
     integer, dimension(0:1) :: timestamp  !!! ADDED
@@ -27,7 +29,7 @@ program test_sor_unroll
 
     niters = 12/UNROLL
 
-    call system_clock(timestamp(0), clock_rate)  !!! MOVED
+    call system_clock(timestamp(0), clock_rate)  !!! ADDED
 
     do iter = 1,niters
         print *,iter
@@ -37,8 +39,8 @@ program test_sor_unroll
     #endif
     end do
 
-    call system_clock(timestamp(1), clock_rate)  !!! MOVED
-    print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)  !!! MOVED
+    call system_clock(timestamp(1), clock_rate)  !!! ADDED
+    print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)  !!! ADDED
 
     ! Copy data from device to host and print
     real, dimension(0:im+1,0:jm+1,0:km+1) :: p0_host
