ea797b8dc77d4deba6c32fbeae1d68afec387243
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 17:17:47 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,22 +1,21 @@
-! test_sor_unroll.f95 
+! test_sor_unroll.f95
 program test_sor_unroll
     use sor_params
-    use sor_routines  !!! ADDED
-    integer :: clock_rate  !!! ADDED
-    integer, dimension(0:1) :: timestamp  !!! ADDED
-
+    use sor_routines
 #ifndef DYN_ALLOC
-    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p0  !!! MODIFIED
-    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p1  !!! MODIFIED
-    real, dimension(0:im+1,0:jm+1,0:km+1), device :: rhs  !!! MODIFIED
+    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p0
+    real, dimension(0:im+1,0:jm+1,0:km+1), device :: p1
+    real, dimension(0:im+1,0:jm+1,0:km+1), device :: rhs
 #else
-    real, device, allocatable  :: p0(:,:,:)  !!! MODIFIED
-    real, device, allocatable  :: p1(:,:,:)  !!! MODIFIED
-    real, device, allocatable  :: rhs(:,:,:)  !!! MODIFIED
+    real, device, allocatable  :: p0(:,:,:)  
+    real, device, allocatable  :: p1(:,:,:)  
+    real, device, allocatable  :: rhs(:,:,:)  
 #endif
     integer :: iter, niters
 
     integer :: i,j,k
+    integer :: clock_rate  !!! ADDED
+    integer, dimension(0:1) :: timestamp  !!! ADDED
 
     do i = 0,im+1
     do j = 0,jm+1
@@ -42,5 +41,8 @@ program test_sor_unroll
     call system_clock(timestamp(1), clock_rate)  !!! ADDED
     print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)  !!! ADDED
 
-    ! print *, p0(im/2,jm/2,km/2)  !!! MODIFIED
+    ! Copy data from device to host and print
+    real, dimension(0:im+1,0:jm+1,0:km+1) :: p0_host
+    p0_host = p0
+    print *, p0_host(im/2,jm/2,km/2)
 end program test_sor_unroll
