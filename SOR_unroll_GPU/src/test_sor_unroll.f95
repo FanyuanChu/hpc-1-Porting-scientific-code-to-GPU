86a8ff0b11e23ea6b2efa49f5d3fb7612a574c71
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 18:52:31 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -2,7 +2,6 @@
 program test_sor_unroll
     use sor_params
     use sor_routines
-
 #ifndef DYN_ALLOC
     real, dimension(0:im+1,0:jm+1,0:km+1), device :: p0
     real, dimension(0:im+1,0:jm+1,0:km+1), device :: p1
@@ -15,8 +14,6 @@ program test_sor_unroll
     integer :: iter, niters
 
     integer :: i,j,k
-    integer :: clock_rate  !!! ADDED
-    integer, dimension(0:1) :: timestamp  !!! ADDED
 
     do i = 0,im+1
     do j = 0,jm+1
@@ -29,12 +26,6 @@ program test_sor_unroll
 
     niters = 12/UNROLL
 
-    ! Copy data from device to host before call system_clock
-    real, dimension(0:im+1,0:jm+1,0:km+1) :: p0_host
-    p0_host = p0
-
-    call system_clock(timestamp(0), clock_rate)  !!! ADDED
-
     do iter = 1,niters
         print *,iter
         call sor (p0,p1,rhs)
@@ -43,12 +34,8 @@ program test_sor_unroll
     #endif
     end do
 
-    ! Copy data from device to host before call system_clock
+    ! Copy data from device to host and print
+    real, dimension(0:im+1,0:jm+1,0:km+1) :: p0_host
     p0_host = p0
-
-    call system_clock(timestamp(1), clock_rate)  !!! ADDED
-    print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)  !!! ADDED
-
-    ! Print
     print *, p0_host(im/2,jm/2,km/2)
 end program test_sor_unroll
