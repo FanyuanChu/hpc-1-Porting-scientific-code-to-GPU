d8355ff75be2126f1a81937790b0d6a143625745
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-31 00:42:35 +0100

    Update sor_routines.f95

--- SOR_unroll_GPU/src/sor_routines.f95
+++ SOR_unroll_GPU/src/sor_routines.f95
@@ -2,7 +2,11 @@ module sor_routines
 use sor_params
 contains
 
-attributes(global) subroutine sor_kernel(p0,p1,rhs)
+type dim3
+    integer :: x, y, z
+end type dim3
+
+attributes(global) subroutine sor_kernel(p0,p1,rhs) 
     use sor_params
     real, dimension(0:im+1,0:jm+1,0:km+1), intent(In), device :: p0
     real, dimension(0:im+1,0:jm+1,0:km+1), intent(Out), device :: p1
@@ -23,7 +27,25 @@ attributes(global) subroutine sor_kernel(p0,p1,rhs)
     k = (blockIdx%z - 1) * blockDim%z + threadIdx%z
 
     if (i > 0 .and. i <= im+1 .and. j > 0 .and. j <= jm+1 .and. k > 0 .and. k <= km+1) then
-        ! ... put the rest of your code here ...
+        if (i==im+1) then
+            p1(i,j,k) = p0(i-im,j,k)
+        else if (i==0) then
+            p1(i,j,k) = p0(i+im,j,k)
+        else if (j==jm+1) then
+            p1(i,j,k)=p0(i-1,j,k)
+        else if (j==0) then
+            p1(i,j,k)=p0(i,j,k)
+        else if (k==0) then
+            p1(i,j,k)=p0(i,j,k)
+        else if (k==km+1) then
+            p1(i,j,k)=p0(i,j,k)
+        else
+            reltmp = omega*(cn1 *(cn2l*p0(i+1,j,k) + &
+                cn2s*p0(i-1,j,k) +cn3l*p0(i,j+1,k) + &
+                cn3s*p0(i,j-1,k) +cn4l*p0(i,j,k+1) + &
+                cn4s*p0(i,j,k-1) -rhs(i,j,k))-p0(i,j,k))
+            p1(i,j,k) = p0(i,j,k) +reltmp    
+        end if
     end if
 end subroutine sor_kernel
 
