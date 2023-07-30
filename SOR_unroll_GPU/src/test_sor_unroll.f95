8139d256ce0fef74729420c846cf2b9c71aed589
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 22:28:27 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,26 +1,16 @@
- program test_sor_unroll
-#ifdef WITH_OPENMP
-use omp_lib
-#endif
+program test_sor_unroll
 
 use sor_params
 use sor_routines
-use cudafor
 
 real, dimension(0:im+1,0:jm+1,0:km+1) :: p0
 real, dimension(0:im+1,0:jm+1,0:km+1) :: p1
 real, dimension(0:im+1,0:jm+1,0:km+1) :: rhs
-real, dimension(0:im+1,0:jm+1,0:km+1), device :: d_p0
-real, dimension(0:im+1,0:jm+1,0:km+1), device :: d_p1
-real, dimension(0:im+1,0:jm+1,0:km+1), device :: d_rhs
+real, dimension(:,:,:), allocatable, device :: d_p0, d_p1, d_rhs
+integer :: blocks(3), threads(3), iter, niters
 
-integer :: iter, niters
-
-integer :: i,j,k
-#ifdef TIMING
-    integer :: clock_rate
-    integer, dimension(0:1) :: timestamp
-#endif
+blocks = [im/32+1, jm/32+1, km/32+1]
+threads = [32, 32, 32]
 
 do i = 0,im+1
 do j = 0,jm+1
@@ -31,35 +21,25 @@ enddo
 enddo
 enddo
 
+allocate(d_p0(0:im+1,0:jm+1,0:km+1))
+allocate(d_p1(0:im+1,0:jm+1,0:km+1))
+allocate(d_rhs(0:im+1,0:jm+1,0:km+1))
+
 d_p0 = p0
 d_p1 = p0
 d_rhs = rhs
 
 niters = 12
-#ifdef WITH_OPENMP
-omp_set_num_threads(4)
-#endif
 do iter = 1,niters
-#ifdef TIMING
-call system_clock(count_rate = clock_rate)
-call system_clock(count = timestamp(0))
-#endif
-#ifdef WITH_OPENMP
-!$omp parallel do
-#endif
     call sor<<<blocks, threads>>>(d_p0,d_p1,d_rhs)
-#ifdef TIMING
-call system_clock(count = timestamp(1))
-print *,'Iteration', iter, 'took', real(timestamp(1) - timestamp(0)) / real(clock_rate), 'seconds.'
-#endif
-if (mod(iter,2) == 0) then
     d_p0 = d_p1
-else
-    d_p1 = d_p0
-endif
 enddo
 p1 = d_p1
 
 print *, 'Result: ', p1(im/2+1,jm/2+1,km/2+1)
+
+deallocate(d_p0)
+deallocate(d_p1)
+deallocate(d_rhs)
 
 end program test_sor_unroll
