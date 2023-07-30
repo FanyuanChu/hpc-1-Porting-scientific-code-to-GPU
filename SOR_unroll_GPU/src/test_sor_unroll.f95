6a318cd5e7177f41ca0ef732cccce653e7a16f46
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 23:35:28 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -9,14 +9,11 @@ program test_sor_unroll
     real, dimension(:,:,:), allocatable :: p0_host
     integer :: iter, niters
     integer :: i,j,k
-    integer(kind=8) :: size
 
     allocate(p0_host(0:im+1,0:jm+1,0:km+1))
-
-    size = sizeof(real)*(im+2)*(jm+2)*(km+2)
-    call cudaMalloc(p0, size)
-    call cudaMalloc(p1, size)
-    call cudaMalloc(rhs, size)
+    allocate(p0(0:im+1,0:jm+1,0:km+1))
+    allocate(p1(0:im+1,0:jm+1,0:km+1))
+    allocate(rhs(0:im+1,0:jm+1,0:km+1))
 
     do i = 0,im+1
         do j = 0,jm+1
@@ -26,7 +23,7 @@ program test_sor_unroll
         end do
     end do
 
-    call cudaMemcpy(p0, p0_host, size, cudaMemcpyHostToDevice)
+    p0 = p0_host
     rhs = p0
 
     niters = 12
@@ -36,12 +33,9 @@ program test_sor_unroll
         p0 = p1
     end do
 
-    call cudaMemcpy(p0_host, p0, size, cudaMemcpyDeviceToHost)
+    p0_host = p0
 
     print *, p0_host(im/2,jm/2,km/2)
 
-    call cudaFree(p0)
-    call cudaFree(p1)
-    call cudaFree(rhs)
-    deallocate(p0_host)
+    deallocate(p0, p1, rhs, p0_host)
 end program test_sor_unroll
