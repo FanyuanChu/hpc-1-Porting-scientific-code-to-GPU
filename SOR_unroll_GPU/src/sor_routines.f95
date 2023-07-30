a75893b82fd15ff9bfe0d9e1a32aa338361f9e02
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 22:28:58 +0100

    Update sor_routines.f95

--- SOR_unroll_GPU/src/sor_routines.f95
+++ SOR_unroll_GPU/src/sor_routines.f95
@@ -1,30 +1,30 @@
 module sor_routines
 use sor_params
-use cudafor
+
 contains
 
-attributes(global) subroutine sor (p0,p1,rhs)
+attributes(global) subroutine sor (d_p0,d_p1,d_rhs)
     use sor_params    
-real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(In) :: p0
-real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(Out) :: p1
-real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(In) :: rhs 
+real, dimension(0:im+1,0:jm+1,0:km+1), intent(In), device :: d_p0
+real, dimension(0:im+1,0:jm+1,0:km+1), intent(Out), device :: d_p1
+real, dimension(0:im+1,0:jm+1,0:km+1), intent(In), device :: d_rhs
 integer :: i,j,k
 
-i = threadIdx%x + (blockIdx%x-1)*blockDim%x
-j = threadIdx%y + (blockIdx%y-1)*blockDim%y
-k = threadIdx%z + (blockIdx%z-1)*blockDim%z
+i = (blockIdx%x-1)*blockDim%x + threadIdx%x
+j = (blockIdx%y-1)*blockDim%y + threadIdx%y
+k = (blockIdx%z-1)*blockDim%z + threadIdx%z
 
-if (i<=im+1 .and. j<=jm+1 .and. k<=km+1) then
-    call sor_kernel(p0,p1,rhs,i,j,k)
-end if
+if(i<=im+1 .and. j<=jm+1 .and. k<=km+1) then
+  call sor_kernel(d_p0,d_p1,d_rhs,i,j,k)
+endif
 
 end subroutine sor
 
-subroutine sor_kernel(p0,p1,rhs,i,j,k) 
+attributes(device) subroutine sor_kernel(d_p0,d_p1,d_rhs,i,j,k) 
     use sor_params
-real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(In) :: p0
-real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(Out) :: p1
-real, dimension(0:im+1,0:jm+1,0:km+1), device, intent(In) :: rhs 
+real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: d_p0
+real, dimension(0:im+1,0:jm+1,0:km+1), intent(Out) :: d_p1
+real, dimension(0:im+1,0:jm+1,0:km+1), intent(In) :: d_rhs 
 integer, intent(In) :: i,j,k
 real(kind=4), parameter :: cn1 = 1.0/3.0
 real(kind=4), parameter :: cn2l = 0.5
@@ -36,25 +36,13 @@ real(kind=4), parameter :: cn4s = 0.5
 real, parameter  :: omega = 1.0
 real :: reltmp
 
-if (i==im+1) then
-    p1(i,j,k) = p0(i-im,j,k)
-else if (i==0) then
-    p1(i,j,k) = p0(i+im,j,k)
-else if (j==jm+1) then
-    p1(i,j,k)=p0(i-1,j,k)
-else if (j==0) then
-    p1(i,j,k)=p0(i,j,k)
-else if (k==0) then
-    p1(i,j,k)=p0(i,j,k)
-else if (k==km+1) then
-    p1(i,j,k)=p0(i,j,k)
-else
-    reltmp = omega*(cn1 *(cn2l*p0(i+1,j,k) + &
-        cn2s*p0(i-1,j,k) +cn3l*p0(i,j+1,k) + &
-        cn3s*p0(i,j-1,k) +cn4l*p0(i,j,k+1) + &
-        cn4s*p0(i,j,k-1) -rhs(i,j,k))-p0(i,j,k))
-    p1(i,j,k) = p0(i,j,k) +reltmp    
-end if
+if(i > 0 .and. i < im+1 .and. j > 0 .and. j < jm+1 .and. k > 0 .and. k < km+1) then
+    reltmp = d_rhs(i,j,k) - cn4l*(d_p0(i+1,j,k) + d_p0(i-1,j,k)) - cn4s*(d_p0(i+2,j,k) + d_p0(i-2,j,k)) &
+        - cn2l*(d_p0(i,j+1,k) + d_p0(i,j-1,k)) - cn2s*(d_p0(i,j+2,k) + d_p0(i,j-2,k)) &
+        - cn3l*(d_p0(i,j,k+1) + d_p0(i,j,k-1)) - cn3s*(d_p0(i,j,k+2) + d_p0(i,j,k-2)) &
+        - cn1*d_p0(i,j,k)
+    d_p1(i,j,k) = d_p0(i,j,k) + omega*reltmp
+endif
 
 end subroutine sor_kernel
 
