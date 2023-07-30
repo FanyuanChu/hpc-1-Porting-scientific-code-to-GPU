c68662c0f88acaba323f643549fadd3fabeaca3e
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 22:36:04 +0100

    Update sor_params.f95

--- SOR_unroll_GPU/src/sor_params.f95
+++ SOR_unroll_GPU/src/sor_params.f95
@@ -3,5 +3,4 @@ module sor_params
 integer, parameter :: im=1000
 integer, parameter :: jm=1000
 integer, parameter :: km=320
-integer, parameter :: blockSize = 256  ! Block size for CUDA kernel
 end module sor_params
