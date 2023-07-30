3bfbe4938fdbc6fbd42bcb2c46da0d57d27da473
FanyuanChu <2705957C@student.gla.ac.uk>
2023-07-30 22:37:51 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,45 +1,39 @@
 program test_sor_unroll
+#ifdef WITH_OPENMP
+use omp_lib
+#endif
 
 use sor_params
 use sor_routines
+real, dimension(:,:,:), device, allocatable :: p0, p1, rhs
+integer :: iter, niters
 
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p0
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p1
-real, dimension(0:im+1,0:jm+1,0:km+1) :: rhs
-real, dimension(:,:,:), allocatable, device :: d_p0, d_p1, d_rhs
-integer :: blocks(3), threads(3), iter, niters
+integer :: i,j,k
 
-blocks = [im/32+1, jm/32+1, km/32+1]
-threads = [32, 32, 32]
+allocate(p0(0:im+1,0:jm+1,0:km+1))
+allocate(p1(0:im+1,0:jm+1,0:km+1))
+allocate(rhs(0:im+1,0:jm+1,0:km+1))
 
 do i = 0,im+1
 do j = 0,jm+1
 do k = 0,km+1
     rhs(i,j,k) = 1.0
     p0(i,j,k) = 1.0
-enddo
-enddo
-enddo
-
-allocate(d_p0(0:im+1,0:jm+1,0:km+1))
-allocate(d_p1(0:im+1,0:jm+1,0:km+1))
-allocate(d_rhs(0:im+1,0:jm+1,0:km+1))
-
-d_p0 = p0
-d_p1 = p0
-d_rhs = rhs
+end do
+end do
+end do
 
 niters = 12
 do iter = 1,niters
-    call sor<<<blocks, threads>>>(d_p0,d_p1,d_rhs)
-    d_p0 = d_p1
-enddo
-p1 = d_p1
+    print *,iter
+    call sor_kernel<<<dim3(im+1,jm+1,km+1),1>>>(p0,p1,rhs,i,j,k)
+    p0=p1
+end do
 
-print *, 'Result: ', p1(im/2+1,jm/2+1,km/2+1)
+print *, p0(im/2,jm/2,km/2)
 
-deallocate(d_p0)
-deallocate(d_p1)
-deallocate(d_rhs)
+deallocate(p0)
+deallocate(p1)
+deallocate(rhs)
 
-end program test_sor_unroll
+end program
