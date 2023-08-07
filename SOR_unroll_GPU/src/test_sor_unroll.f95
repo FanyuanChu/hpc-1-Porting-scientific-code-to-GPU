e8d18104fcd3b05dcf2ea97967417c2dbbd1dd3e
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-07 03:44:04 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,61 +1,62 @@
-! test_sor_unroll.f95 second
 program test_sor_unroll
-use cudafor
-use sor_params
-use sor_routines
+    use cudafor
+    use sor_params
+    use sor_routines
 
-integer, parameter :: UNROLL = 4
-real, device, allocatable :: p0(:,:,:), p1(:,:,:), p2(:,:,:), p3(:,:,:), p4(:,:,:), rhs(:,:,:)
-real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:)
-integer :: iter, niters
-integer :: i,j,k
-real :: start_time, end_time
-real :: sample
+    integer, parameter :: UNROLL = 4
+    real, device, allocatable :: p0(:,:,:), p1(:,:,:), p2(:,:,:), p3(:,:,:), p4(:,:,:), rhs(:,:,:)
+    real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:)
+    integer :: iter, niters
+    integer :: i,j,k
+    type(cudaEvent) :: start_event, stop_event
+    real :: elapsed_time
 
-! Allocate device memory
-allocate(p0(0:im+1,0:jm+1,0:km+1))
-allocate(p1(0:im+1,0:jm+1,0:km+1))
-allocate(p2(0:im+1,0:jm+1,0:km+1))
-allocate(p3(0:im+1,0:jm+1,0:km+1))
-allocate(p4(0:im+1,0:jm+1,0:km+1))
-allocate(rhs(0:im+1,0:jm+1,0:km+1))
+    ! Allocate device memory
+    allocate(p0(0:im+1,0:jm+1,0:km+1))
+    allocate(p1(0:im+1,0:jm+1,0:km+1))
+    allocate(rhs(0:im+1,0:jm+1,0:km+1))
+    allocate(p2(0:im+1,0:jm+1,0:km+1))
+    allocate(p3(0:im+1,0:jm+1,0:km+1))
+    allocate(p4(0:im+1,0:jm+1,0:km+1))
 
-! Initialization on the host
-allocate(rhs_host(0:im+1,0:jm+1,0:km+1))
-allocate(p0_host(0:im+1,0:jm+1,0:km+1))
-rhs_host = 1.0
-p0_host = 1.0
+    ! Initialization on the host
+    allocate(rhs_host(0:im+1,0:jm+1,0:km+1))
+    allocate(p0_host(0:im+1,0:jm+1,0:km+1))
+    rhs_host = 1.0
+    p0_host = 1.0
 
-! Copy data to the device
-rhs = rhs_host
-p0 = p0_host
+    ! Copy data to the device
+    rhs = rhs_host
+    p0 = p0_host
 
-niters = 12 / UNROLL
-start_time = rtc()
-do iter = 1, niters
-    print *, iter
-    call sor(p0, p1, rhs)
-    call sor(p1, p2, rhs)
-    call sor(p2, p3, rhs)
-    call sor(p3, p4, rhs)
-    p0 = p4
-end do
-end_time = rtc()
+    niters = 12 / UNROLL
+    call cudaEventCreate(start_event)
+    call cudaEventCreate(stop_event)
+    call cudaEventRecord(start_event, 0)
+    do iter = 1, niters
+        print *, iter
+        call sor(p0, p1, rhs)
+        call sor(p1, p2, rhs)
+        call sor(p2, p3, rhs)
+        call sor(p3, p4, rhs)
+        p0 = p4
+    end do
+    call cudaEventRecord(stop_event, 0)
+    call cudaEventSynchronize(stop_event)
+    call cudaEventElapsedTime(elapsed_time, start_event, stop_event)
 
-! Printing a sample value
+    ! Printing a sample value
+    print *, p0(im/2,jm/2,km/2)
+    print *, "Time elapsed: ", elapsed_time / 1000.0, " seconds."
 
-sample = p0(im/2,jm/2,km/2)
-print *, sample
-print *, "Time elapsed: ", end_time - start_time, " seconds."
-
-! Deallocate device memory
-deallocate(p0)
-deallocate(p1)
-deallocate(p2)
-deallocate(p3)
-deallocate(p4)
-deallocate(rhs)
-deallocate(rhs_host)
-deallocate(p0_host)
+    ! Deallocate device memory
+    deallocate(p0)
+    deallocate(p1)
+    deallocate(rhs)
+    deallocate(p2)
+    deallocate(p3)
+    deallocate(p4)
+    deallocate(rhs_host)
+    deallocate(p0_host)
 
 end program
