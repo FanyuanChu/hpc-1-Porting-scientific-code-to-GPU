c210545489610e5c2a0da05de529fc6bc95ea23d
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-07 03:22:38 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,117 +1,57 @@
+! test_sor_unroll.f95 second
 ! test_sor_unroll.f95
 program test_sor_unroll
-#ifdef WITH_OPENMP
-use omp_lib
-#endif
-
+use cudafor
 use sor_params
 use sor_routines
-#ifndef DYN_ALLOC
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p0
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p1
-real, dimension(0:im+1,0:jm+1,0:km+1) :: rhs
-#if UNROLL>1
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p2
-#if UNROLL>2
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p3
-#if UNROLL>3
-real, dimension(0:im+1,0:jm+1,0:km+1) :: p4
-#endif
-#endif
-#endif
-#else
-    real, allocatable  :: p0(:,:,:)
-    real, allocatable  :: p1(:,:,:)
-    real, allocatable  :: rhs(:,:,:)
-#if UNROLL>1
-    real, allocatable  :: p2(:,:,:)
-#if UNROLL>2
-    real, allocatable  :: p3(:,:,:)
-#if UNROLL>3
-    real, allocatable  :: p4(:,:,:)
-#endif
-#endif
-#endif
-#endif
+
+integer, parameter :: UNROLL = 4
+real, device, allocatable  :: p0(:,:,:), p1(:,:,:), p2(:,:,:), p3(:,:,:), p4(:,:,:), rhs(:,:,:)
 integer :: iter, niters
-
 integer :: i,j,k
-#ifdef TIMING
-    integer :: clock_rate
-    integer, dimension(0:1) :: timestamp
-#endif
+real :: start_time, end_time
 
-#ifdef DYN_ALLOC
-    allocate(p0(0:im+1,0:jm+1,0:km+1))
-    allocate(p1(0:im+1,0:jm+1,0:km+1))
-    allocate(rhs(0:im+1,0:jm+1,0:km+1))
-#if UNROLL>1
-    allocate(p2(0:im+1,0:jm+1,0:km+1))
-#if UNROLL>2
-    allocate(p3(0:im+1,0:jm+1,0:km+1))
-#if UNROLL>3
-    allocate(p4(0:im+1,0:jm+1,0:km+1))
-#endif
-#endif
-#endif
-#endif
+allocate(p0(0:im+1,0:jm+1,0:km+1))
+allocate(p1(0:im+1,0:jm+1,0:km+1))
+allocate(rhs(0:im+1,0:jm+1,0:km+1))
+allocate(p2(0:im+1,0:jm+1,0:km+1))
+allocate(p3(0:im+1,0:jm+1,0:km+1))
+allocate(p4(0:im+1,0:jm+1,0:km+1))
 
-do i = 0,im+1
-do j = 0,jm+1
-do k = 0,km+1
-    rhs(i,j,k) = 1.0
-    p0(i,j,k) = 1.0
-end do
-end do
-end do
+! Initialization on the host
+real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:)
+allocate(rhs_host(0:im+1,0:jm+1,0:km+1))
+allocate(p0_host(0:im+1,0:jm+1,0:km+1))
+rhs_host = 1.0
+p0_host = 1.0
+rhs = rhs_host
+p0 = p0_host
 
-niters = 12/UNROLL
-#ifdef TIMING
-    call system_clock(timestamp(0), clock_rate)
-#endif
-do iter = 1,niters
-    print *,iter
-!$RF4A Subroutine sor_superkernel
-!$RF4A Begin Inline
-call sor (p0,p1,rhs)
-#if UNROLL > 1
-call sor (p1,p2,rhs)
-#if UNROLL > 2
-call sor (p2,p3,rhs)
-#if UNROLL > 3
-call sor (p3,p4,rhs)
-#endif
-#endif
-#endif
-!$RF4A End Inline
-!$RF4A End Subroutine sor_superkernel
-#if UNROLL==1
-p0=p1
-#elif UNROLL==2
-p0=p2
-#elif UNROLL==3
-p0=p3
-#elif UNROLL==4
-p0=p4
-#endif
+niters = 12 / UNROLL
+start_time = rtc()
+do iter = 1, niters
+    print *, iter
+    call sor(p0, p1, rhs)
+    call sor(p1, p2, rhs)
+    call sor(p2, p3, rhs)
+    call sor(p3, p4, rhs)
+    p0 = p4
 end do
-#ifdef TIMING
-    call system_clock(timestamp(1), clock_rate)
-    print '(f8.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)
-#endif
-print *, p0(im/2,jm/2,km/2)
-#ifdef DYN_ALLOC
-    deallocate(p0)
-    deallocate(p1)
-    deallocate(rhs)
-#if UNROLL>1
-    deallocate(p2)
-#if UNROLL>2
-    deallocate(p3)
-#if UNROLL>3
-    deallocate(p4)
-#endif
-#endif
-#endif
-#endif
+end_time = rtc()
+
+! Printing a sample value
+real :: sample
+sample = p0(im/2,jm/2,km/2)
+print *, sample
+print *, "Time elapsed: ", end_time - start_time, " seconds."
+
+deallocate(p0)
+deallocate(p1)
+deallocate(rhs)
+deallocate(rhs_host)
+deallocate(p0_host)
+deallocate(p2)
+deallocate(p3)
+deallocate(p4)
+
 end program
