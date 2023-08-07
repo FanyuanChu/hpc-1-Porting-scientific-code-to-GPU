7911d84ac8f755398b24f89ad67a11775013d8d0
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-07 03:32:00 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -1,29 +1,33 @@
 ! test_sor_unroll.f95 second
+! test_sor_unroll.f95
 program test_sor_unroll
 use cudafor
 use sor_params
 use sor_routines
 
 integer, parameter :: UNROLL = 4
-real, device, allocatable  :: p0(:,:,:), p1(:,:,:), p2(:,:,:), p3(:,:,:), p4(:,:,:), rhs(:,:,:)
+real, device, allocatable :: p0(:,:,:), p1(:,:,:), p2(:,:,:), p3(:,:,:), p4(:,:,:), rhs(:,:,:)
+real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:)
 integer :: iter, niters
 integer :: i,j,k
 real :: start_time, end_time
 real :: sample
 
+! Allocate device memory
 allocate(p0(0:im+1,0:jm+1,0:km+1))
 allocate(p1(0:im+1,0:jm+1,0:km+1))
-allocate(rhs(0:im+1,0:jm+1,0:km+1))
 allocate(p2(0:im+1,0:jm+1,0:km+1))
 allocate(p3(0:im+1,0:jm+1,0:km+1))
 allocate(p4(0:im+1,0:jm+1,0:km+1))
+allocate(rhs(0:im+1,0:jm+1,0:km+1))
 
 ! Initialization on the host
-real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:)
 allocate(rhs_host(0:im+1,0:jm+1,0:km+1))
 allocate(p0_host(0:im+1,0:jm+1,0:km+1))
 rhs_host = 1.0
 p0_host = 1.0
+
+! Copy data to the device
 rhs = rhs_host
 p0 = p0_host
 
@@ -45,13 +49,14 @@ sample = p0(im/2,jm/2,km/2)
 print *, sample
 print *, "Time elapsed: ", end_time - start_time, " seconds."
 
+! Deallocate device memory
 deallocate(p0)
 deallocate(p1)
-deallocate(rhs)
-deallocate(rhs_host)
-deallocate(p0_host)
 deallocate(p2)
 deallocate(p3)
 deallocate(p4)
+deallocate(rhs)
+deallocate(rhs_host)
+deallocate(p0_host)
 
 end program
