cb05c94b5e36344ab8b4127c98dde7ae09737ce5
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-07 03:58:58 +0100

    Update test_sor_unroll.f95

--- SOR_unroll_GPU/src/test_sor_unroll.f95
+++ SOR_unroll_GPU/src/test_sor_unroll.f95
@@ -5,18 +5,23 @@ program test_sor_unroll
 
     integer, parameter :: UNROLL = 4
     real, device, allocatable :: p0(:,:,:), p1(:,:,:), p2(:,:,:), p3(:,:,:), p4(:,:,:), rhs(:,:,:)
-    real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:), sample_host(:,:,:)
+    real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:)
     integer :: iter, niters
     integer :: i,j,k
-    real :: start_time, end_time
+    real :: elapsed_time
+    type(cudaEvent) :: start_event, stop_event
+
+    ! Create CUDA events
+    call cudaEventCreate(start_event)
+    call cudaEventCreate(stop_event)
 
     ! Allocate device memory
     allocate(p0(0:im+1,0:jm+1,0:km+1))
     allocate(p1(0:im+1,0:jm+1,0:km+1))
-    allocate(rhs(0:im+1,0:jm+1,0:km+1))
     allocate(p2(0:im+1,0:jm+1,0:km+1))
     allocate(p3(0:im+1,0:jm+1,0:km+1))
     allocate(p4(0:im+1,0:jm+1,0:km+1))
+    allocate(rhs(0:im+1,0:jm+1,0:km+1))
 
     ! Initialization on the host
     allocate(rhs_host(0:im+1,0:jm+1,0:km+1))
@@ -29,7 +34,10 @@ program test_sor_unroll
     p0 = p0_host
 
     niters = 12 / UNROLL
-    start_time = rtc()
+
+    ! Record the start event
+    call cudaEventRecord(start_event, 0)
+
     do iter = 1, niters
         print *, iter
         call sor(p0, p1, rhs)
@@ -38,24 +46,30 @@ program test_sor_unroll
         call sor(p3, p4, rhs)
         p0 = p4
     end do
-    end_time = rtc()
 
-    ! Copy sample value back to host
-    allocate(sample_host(0:im+1,0:jm+1,0:km+1))
-    sample_host = p0
+    ! Record the stop event
+    call cudaEventRecord(stop_event, 0)
+    call cudaEventSynchronize(stop_event)
+
+    ! Compute the elapsed time between events
+    call cudaEventElapsedTime(elapsed_time, start_event, stop_event)
 
     ! Printing a sample value
-    print *, sample_host(im/2,jm/2,km/2)
-    print *, "Time elapsed: ", (end_time - start_time) / 1.0e9, " seconds."
+    print *, p0(im/2,jm/2,km/2)
+    print *, "Time elapsed: ", elapsed_time / 1000.0, " seconds."
 
     ! Deallocate device memory
     deallocate(p0)
     deallocate(p1)
-    deallocate(rhs)
     deallocate(p2)
     deallocate(p3)
     deallocate(p4)
+    deallocate(rhs)
     deallocate(rhs_host)
     deallocate(p0_host)
-    deallocate(sample_host)
+
+    ! Destroy CUDA events
+    call cudaEventDestroy(start_event)
+    call cudaEventDestroy(stop_event)
+
 end program
