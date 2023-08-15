b38d6008c5f23ea0d1c3142b2b293242a86a933e
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-15 05:33:14 +0100

    Update test_sor_unroll_time.f95

--- SOR_unroll_GPU/src/test_sor_unroll_time.f95
+++ SOR_unroll_GPU/src/test_sor_unroll_time.f95
@@ -7,15 +7,13 @@ program test_sor_unroll
   real, device, allocatable :: p0(:,:,:), p1(:,:,:), p2(:,:,:), p3(:,:,:), p4(:,:,:), rhs(:,:,:)
   real, allocatable :: rhs_host(:,:,:), p0_host(:,:,:)
   integer :: iter, niters
-  integer :: i,j,k
-  real :: elapsed_time, init_time, copy_to_device_time, comp_time, copy_from_device_time, total_time
-  type (cudaEvent) :: start_event, stop_event, init_start_event, init_stop_event, copy_to_device_start_event, copy_to_device_stop_event, comp_start_event, comp_stop_event, copy_from_device_start_event, copy_from_device_stop_event
+  integer :: i, j, k
+  real :: init_time, copy_to_device_time, comp_time, copy_from_device_time, total_time
+  type (cudaEvent) :: init_start_event, init_stop_event, copy_to_device_start_event, copy_to_device_stop_event, comp_start_event, comp_stop_event, copy_from_device_start_event, copy_from_device_stop_event, total_start_event, total_stop_event
   integer :: istat
   real :: sample_device, sample_host
 
-  ! Create events for all timings
-  istat = cudaEventCreate(start_event)
-  istat = cudaEventCreate(stop_event)
+  ! Create all events
   istat = cudaEventCreate(init_start_event)
   istat = cudaEventCreate(init_stop_event)
   istat = cudaEventCreate(copy_to_device_start_event)
@@ -24,11 +22,12 @@ program test_sor_unroll
   istat = cudaEventCreate(comp_stop_event)
   istat = cudaEventCreate(copy_from_device_start_event)
   istat = cudaEventCreate(copy_from_device_stop_event)
+  istat = cudaEventCreate(total_start_event)
+  istat = cudaEventCreate(total_stop_event)
 
-  ! Record total start time
-  istat = cudaEventRecord(start_event, 0)
+  istat = cudaEventRecord(total_start_event, 0)
 
-  ! Measure initialization time on the host
+  ! Initialization on the host
   istat = cudaEventRecord(init_start_event, 0)
   allocate(rhs_host(0:im+1,0:jm+1,0:km+1))
   allocate(p0_host(0:im+1,0:jm+1,0:km+1))
@@ -36,18 +35,29 @@ program test_sor_unroll
   p0_host = 1.0
   istat = cudaEventRecord(init_stop_event, 0)
   istat = cudaEventSynchronize(init_stop_event)
+  istat = cudaEventElapsedTime(init_time, init_start_event, init_stop_event)
 
-  ! Measure data copy time to the device
+  ! Allocate device memory
+  allocate(p0(0:im+1,0:jm+1,0:km+1))
+  allocate(p1(0:im+1,0:jm+1,0:km+1))
+  allocate(p2(0:im+1,0:jm+1,0:km+1))
+  allocate(p3(0:im+1,0:jm+1,0:km+1))
+  allocate(p4(0:im+1,0:jm+1,0:km+1))
+  allocate(rhs(0:im+1,0:jm+1,0:km+1))
+
+  ! Copy data to the device
   istat = cudaEventRecord(copy_to_device_start_event, 0)
   rhs = rhs_host
   p0 = p0_host
   istat = cudaEventRecord(copy_to_device_stop_event, 0)
   istat = cudaEventSynchronize(copy_to_device_stop_event)
+  istat = cudaEventElapsedTime(copy_to_device_time, copy_to_device_start_event, copy_to_device_stop_event)
 
   niters = 12 / UNROLL
 
-  ! Measure computation time on the device
+  ! Record the start time for computation
   istat = cudaEventRecord(comp_start_event, 0)
+
   do iter = 1, niters
     print *, iter
     call sor(p0, p1, rhs)
@@ -56,27 +66,29 @@ program test_sor_unroll
     call sor(p3, p4, rhs)
     p0 = p4
   end do
+
+  ! End the computation time recording
   istat = cudaEventRecord(comp_stop_event, 0)
   istat = cudaEventSynchronize(comp_stop_event)
+  istat = cudaEventElapsedTime(comp_time, comp_start_event, comp_stop_event)
 
-  ! Measure data copy time from device to host
+  ! Get a sample value from the device
+  sample_device = p0(im/2,jm/2,km/2)
+
+  ! Copy from device to host
   istat = cudaEventRecord(copy_from_device_start_event, 0)
-  sample_host = p0(im/2,jm/2,km/2)
+  sample_host = sample_device  ! Copy the value to the host
   istat = cudaEventRecord(copy_from_device_stop_event, 0)
   istat = cudaEventSynchronize(copy_from_device_stop_event)
-
-  ! Record total stop time
-  istat = cudaEventRecord(stop_event, 0)
-  istat = cudaEventSynchronize(stop_event)
-
-  ! Calculate elapsed times for all sections
-  istat = cudaEventElapsedTime(init_time, init_start_event, init_stop_event)
-  istat = cudaEventElapsedTime(copy_to_device_time, copy_to_device_start_event, copy_to_device_stop_event)
-  istat = cudaEventElapsedTime(comp_time, comp_start_event, comp_stop_event)
   istat = cudaEventElapsedTime(copy_from_device_time, copy_from_device_start_event, copy_from_device_stop_event)
-  istat = cudaEventElapsedTime(total_time, start_event, stop_event)
 
-  ! Printing timings
+  ! Record the total time
+  istat = cudaEventRecord(total_stop_event, 0)
+  istat = cudaEventSynchronize(total_stop_event)
+  istat = cudaEventElapsedTime(total_time, total_start_event, total_stop_event)
+
+  ! Printing results
+  print *, sample_host
   print *, "Initialization time: ", init_time / 1000.0, " seconds."
   print *, "Time to copy data to the device: ", copy_to_device_time / 1000.0, " seconds."
   print *, "Time for computation on the device: ", comp_time / 1000.0, " seconds."
@@ -93,9 +105,7 @@ program test_sor_unroll
   deallocate(rhs_host)
   deallocate(p0_host)
 
-  ! Destroy events
-  istat = cudaEventDestroy(start_event)
-  istat = cudaEventDestroy(stop_event)
+  ! Destroy all events
   istat = cudaEventDestroy(init_start_event)
   istat = cudaEventDestroy(init_stop_event)
   istat = cudaEventDestroy(copy_to_device_start_event)
@@ -104,5 +114,7 @@ program test_sor_unroll
   istat = cudaEventDestroy(comp_stop_event)
   istat = cudaEventDestroy(copy_from_device_start_event)
   istat = cudaEventDestroy(copy_from_device_stop_event)
+  istat = cudaEventDestroy(total_start_event)
+  istat = cudaEventDestroy(total_stop_event)
 
 end program
