0ffa82b94b28acc77cc7955ba8fc53b6bb2b5869
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-22 02:43:45 +0100

    Update gen_sor_superkernel_time.f95

--- SOR_unroll_GPU/unroll_3/gen_sor_superkernel_time.f95
+++ SOR_unroll_GPU/unroll_3/gen_sor_superkernel_time.f95
@@ -28,8 +28,8 @@ program main
     !integer, parameter :: niters=12
     !integer, parameter :: UNROLL = 4
     !integer, parameter :: UNROLL = 3
-    !integer, parameter :: UNROLL = 2
-    integer, parameter :: UNROLL = 1
+    integer, parameter :: UNROLL = 2
+    !integer, parameter :: UNROLL = 1
     
     integer, parameter :: blockSize = 256
 
