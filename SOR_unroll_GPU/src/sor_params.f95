367652f07e7a677531da59deebda0baa8151d766
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-23 23:41:20 +0100

    Update sor_params.f95

--- SOR_unroll_GPU/src/sor_params.f95
+++ SOR_unroll_GPU/src/sor_params.f95
@@ -1,9 +1,13 @@
 ! sor_params.f95
 module sor_params
 use cudafor
-integer, parameter :: im=1000
-integer, parameter :: jm=1000
-integer, parameter :: km=320
+integer, parameter :: im=1500
+integer, parameter :: jm=1500
+integer, parameter :: km=480
+
+!integer, parameter :: im=1000
+!integer, parameter :: jm=1000
+!integer, parameter :: km=320
 
 !integer, parameter :: im=500
 !integer, parameter :: jm=500
