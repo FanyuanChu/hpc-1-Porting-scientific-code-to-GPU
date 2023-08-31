bacd722079a87678eaf6904601e2b6ebcd1aa16c
FanyuanChu <2705957C@student.gla.ac.uk>
2023-08-31 14:16:38 +0100

    Create sor_params.f95

---
+++ SOR_unroll_GPU_CUDA/src/sor_params.f95
@@ -0,0 +1,27 @@
+! sor_params.f95
+module sor_params
+use cudafor
+integer, parameter :: im=1000
+integer, parameter :: jm=1000
+integer, parameter :: km=480
+
+!integer, parameter :: im=1000
+!integer, parameter :: jm=1000
+!integer, parameter :: km=320
+
+!integer, parameter :: im=500
+!integer, parameter :: jm=500
+!integer, parameter :: km=320
+
+!integer, parameter :: im=200
+!integer, parameter :: jm=200
+!integer, parameter :: km=320
+
+!integer, parameter :: im=200
+!integer, parameter :: jm=200
+!integer, parameter :: km=160
+
+!integer, parameter :: im=100
+!integer, parameter :: jm=100
+!integer, parameter :: km=80
+end module sor_params
